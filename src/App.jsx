d4903890e4b80c80324090eb3772c9caa313b765
Jhonattan Benitez Ballesteros <jhonattan.benitez@hotmail.com>
2023-01-17 19:03:21 -0500

    using props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react"
 import Header from "./components/Header"
 import Form from "./components/Form"
 import PatientList from "./components/PatientList"
 
 const App = () => {
+
+  const [patients, setPatients] = useState([]);
+
   return (
     <div className="container mx-auto mt-10">
-      <Header />
+      <Header 
+
+      />
       <div className="mt-12 md:flex">
-        <Form />
+        <Form 
+          patients={patients}
+          setPatients={setPatients}
+        />
         <PatientList />
       </div>
      
